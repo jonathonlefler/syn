45bf43e9a73a79963c3471ce6928f7ccda367362
jonathonlefler <lefler.t.jonathon@gmail.com>
2018-08-24 20:43:36 -0400

    added varriable decalration, printing, and if else statment

---
+++ java/HelloWorld.java
@@ -0,0 +1,22 @@
+// Java
+
+/* Multiline comment */
+// Single line comment
+
+public class HelloWorld{
+	public static void main(String[] args){
+		String string; int number; double numberDouble; boolean boolStatement = true;
+
+		System.out.println("Hello World!");
+
+		if(boolStatement){
+			System.out.print("if");
+		}
+		else if(boolStatement){
+			System.out.println("else if");
+		}
+		else{
+			System.out.println("else");
+		}
+	}
+}
